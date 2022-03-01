d5fa8267d0946ddd9ce683c25642fb4b4b965ccd
Marton David Orosz <mdox@mdox.xyz>
2022-03-01 19:21:26 +0100

    Reset Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,5 @@
-import useModal from "components/Modal";
-import { useState } from "react";
-
 const HomePage = () => {
-  const { Modal, toggleModal, hideModal, isModalShow } = useModal({
-    hideOnClick: true,
-    hideOnEscape: true,
-    onHide: (hideType) => {
-      console.log(hideType);
-    },
-  });
-
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="prose">
-      <h1>Home Page</h1>
-
-      <button onClick={toggleModal}>
-        {isModalShow ? "Hide" : "Show"} Modal
-      </button>
-
-      <Modal>
-        <div className="flex w-56 flex-col space-y-1 rounded bg-purple-50 p-2 shadow">
-          <span className="text-lg">Modal</span>
-          <hr />
-          <span>Content...</span>
-          <button onClick={() => setCount(count + 1)}>
-            Click to Count: {count}
-          </button>
-          <hr />
-          <button className="self-end" onClick={hideModal}>
-            Close
-          </button>
-        </div>
-      </Modal>
-    </div>
-  );
+  return <div className="prose">Home Page</div>;
 };
 
 export default HomePage;
