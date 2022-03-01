118510faddc1e38b1cbb58d6ed22f099be05aa19
Marton David Orosz <mdox@mdox.xyz>
2022-03-01 16:49:18 +0100

    Base

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss/colors')} */
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
